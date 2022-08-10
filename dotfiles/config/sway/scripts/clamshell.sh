12d625ce81b997b07666e8ff65e3650fc51b37e3
Juan Pablo Morelli <jpmorelli@gmail.com>
2022-08-10 20:22:26 -0300

    New sway, waybar and wofi config

--- dotfiles/config/sway/scripts/clamshell.sh
+++ dotfiles/config/sway/scripts/clamshell.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-if cat /proc/acpi/button/lid/*/state | grep -q open; then
-    swaymsg output <eDP-1> enable
+LAPTOP=eDP-1
+if grep -q open /proc/acpi/button/lid/LID/state; then
+    swaymsg output $LAPTOP enable
 else
-    swaymsg output <eDP-1> disable
+    swaymsg output $LAPTOP disable
 fi
--- dotfiles/config/sway/scripts/screenshot.sh
+++ dotfiles/config/sway/scripts/screenshot.sh
@@ -1,18 +1,13 @@
-#!/bin/bash
- 
-entries="Active Screen Output Area Window"
- 
-selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widgets.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
- 
-case $selected in
-  active)
-    /usr/share/sway/scripts/grimshot --notify save active;;
-  screen)
-    /usr/share/sway/scripts/grimshot --notify save screen;;
-  output)
-    /usr/share/sway/scripts/grimshot --notify save output;;
-  area)
-    /usr/share/sway/scripts/grimshot --notify save area;;
-  window)
-    /usr/share/sway/scripts/grimshot --notify save window;;
-esac
+#!/bin/sh
+location="$HOME/Pictures/Screenshots/"
+filename="$(date +%F\ %T).png"
+geometry="$(slurp)"
+fullpath="$location$filename"
+mkdir -p "$location"
+if [ ! "$geometry" ]; then
+    sleep 1
+    grim -c "$fullpath"
+else
+    grim -g "$geometry" "$fullpath"
+fi
+notify-send "Screenshot saved to '$fullpath'"
